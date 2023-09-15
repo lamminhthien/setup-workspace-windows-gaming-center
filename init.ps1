6fb141a3545a5a62c385904f9fadee7040946f09
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-09-15 13:57:48 +0000

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -15,13 +15,6 @@ choco install nvm -y
 git config --global user.email "lamminhthien02012000@gmail.com"
 git config --global user.name "Thien Lam"
 
-# Download nvm for windows
-# $urlNVM = "https://github.com/coreybutler/nvm-windows/releases/download/1.1.11/nvm-setup.exe"
-# $outputPathNVM = "C:\nvm-setup.exe"
-# Invoke-WebRequest -Uri $urlNVM -OutFile $outputPathNVM
-# cd C:\
-# .\nvm-setup.exe
-
 # Setup ssh key
 # ssh-keygen
 
