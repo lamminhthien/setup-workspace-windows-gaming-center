9790f3398455cf0a168c9895fbd1f0e195200b3f
Thien Lam <lamminhthien02012000@gmail.com>
2023-09-15 20:21:38 +0700

    push

--- init.ps1
+++ init.ps1
@@ -1,11 +1,17 @@
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Install VS Code
-choco install vscode -y
-
 # Install Git
 choco install git -y
 
+# Install VS Code
+choco install vscode -y
+
+# Install NVM
+choco install nvm -y
+
 # Install Docker Desktop
 choco install docker-desktop
+
+# Setup ssh key
+ssh-keygen
